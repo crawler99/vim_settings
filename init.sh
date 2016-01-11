3461656d2928de443252d5311317c87e4ba877e1
crawler99 <taojiang99@gmail.com>
2016-01-11 16:52:11 +0800

    small fix

--- init.sh
+++ init.sh
@@ -7,7 +7,7 @@ VIM_HOME=$HOME/.vim
 VUNDLE_HOME=$VIM_HOME/bundle/Vundle.vim
 VUNDLE_REPO=https://github.com/VundleVim/Vundle.vim.git
 
-if [ ! -f $VUNDLE_HOME ]; then
+if [ ! -d $VUNDLE_HOME ]; then
     echo "creating dir for $VUNDLE_HOME ..."
     mkdir -p $VUNDLE_HOME
     git clone $VUNDLE_REPO $VUNDLE_HOME
@@ -18,7 +18,7 @@ echo "Setting up the color scheme for the first use ..."
 COLOR_SCHEMES=$VIM_HOME/colors
 COLOR_SCHEMES_REPO=$HOME/github/vim_settings/colors
 
-if [ ! -f $COLOR_SCHEMES ]; then
+if [ ! -d $COLOR_SCHEMES ]; then
     echo "linking color schemes to $COLOR_SCHEMES ..."
     ln -s $COLOR_SCHEMES_REPO $COLOR_SCHEMES
 fi
